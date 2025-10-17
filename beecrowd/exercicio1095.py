222c321d16701f1fd3dda023ddd8fb6d1080434f
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-10-17 15:55:59 -0300

    Envio de resolução parcial da questão 1096

---
+++ beecrowd/exercicio1095.py
@@ -0,0 +1,13 @@
+#Sequencia IJ 1
+
+i = 1
+j = 60
+
+for num in range(j, -5, -5):
+
+    print("I={:.0f} J={:.0f}".format(i, j))
+    
+    j -= 5
+    i += 3
+
+    
---
+++ beecrowd/exercicio1096.py
@@ -0,0 +1,12 @@
+# Sequencia IJ 2
+
+i = 1
+j = 7
+
+for num in range(i, 9):
+
+    #for() Esse for vai printar 3 vezes
+    print("I={:.0f} J={:.0f}".format(i, j))
+    
+    j -= 5
+    i += 3
