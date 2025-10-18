c144f9f49b50ee878609bfc651c8b22a965b558b
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-10-18 16:36:46 -0300

    Envio de resolução de problema 1096 do beecrowd

--- beecrowd/exercicio1096.py
+++ beecrowd/exercicio1096.py
@@ -1,12 +1,13 @@
 # Sequencia IJ 2
 
 i = 1
-j = 7
 
-for num in range(i, 9):
+for num in range(i, 10, 2):
 
-    #for() Esse for vai printar 3 vezes
-    print("I={:.0f} J={:.0f}".format(i, j))
-    
-    j -= 5
-    i += 3
+    j = 7
+
+    for cont in range(3):
+        print("I={:.0f} J={:.0f}".format(i, j))
+        j -= 1
+
+    i += 2
