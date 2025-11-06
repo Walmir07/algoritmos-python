36f7e1bc77480fe59e175dcb92be1f6d39f3b686
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-11-06 08:34:25 -0300

    Envio de resolução parcial da questão 1179 beecrowd

--- beecrowd/exercicio1179.py
+++ beecrowd/exercicio1179.py
@@ -1,23 +1,22 @@
 pares = []
 impares = []
 
-for i in range(0, 15):
+for i in range(10):
+
     valor = int(input())
     
     if valor / 2 == 0:
         pares.append(valor)
+        if(len(pares) == 5):
+            for j in range(5):
+                print(f"par[{j}] = {pares[j]}")
+
+            pares.clear()
     else:
         impares.append(valor)
-    
-    for j in range(5):
-        print(f"par[{j}] = {pares[j]}")
-    
-    for r in range(5):
-        print(f"impar[{r}] = {impares[r]}")
-    
-    if len(pares) == 5:
-        pares.clear()
-        
-    if len(impares) == 5:
-        impares.clear()
+        if(len(impares) == 5):
+            for k in range(5):
+                print(f"impar[{k}] = {impares[k]}")
+
+            pares.clear()
     
